ec4605a3cef3766a177ded9f275356b81e0a171c
Robert Freihaut <rfreihaut@aol.com>
2019-06-15 23:17:23 -0400

    create currency converter

--- CurrencyFormatConverter/Program.cs
+++ CurrencyFormatConverter/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace CurrencyFormatConverter
 {
     class Program
     {
+        static double firstNumber, secondNumber, thirdNumber;
+        static double average, largest, smallest, total;
         static void Main(string[] args)
         {
+            ConvertCurrency();
+        }
+
+        static void GetInputs()
+        {
+            Console.WriteLine("Please enter first amount ($):");
+            firstNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter second amount ($):");
+            secondNumber = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter third amount ($):");
+            thirdNumber = double.Parse(Console.ReadLine());
+        }
+
+        static void ConvertCurrency()
+        {
+            GetInputs();
+
+            average = (firstNumber + secondNumber + thirdNumber) / 3;
+            smallest = Math.Min(firstNumber, Math.Min(secondNumber, thirdNumber));
+            largest = Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber));
+            total = firstNumber + secondNumber + thirdNumber;
+
+            Print();
+        }
+
+        static void Print()
+        {
+            Console.WriteLine($"Average is: {average}");
+            Console.WriteLine($"Smallest is: {smallest}");
+            Console.WriteLine($"Largest is: {largest}");
+            Console.WriteLine("Total is:");
+            Console.WriteLine($"\tUS: {FormatCurrency(total, "en-US")}");
+            Console.WriteLine($"\tSwedish: {FormatCurrency(total, "sv-SE")}");
+            Console.WriteLine($"\tJapanese: {FormatCurrency(total, "ja-JP")}");
+            Console.WriteLine($"\tThai: {FormatCurrency(total, "th-TH")}");
+            Console.ReadKey();
+        }
+
+        static string FormatCurrency(double totalAmount, string cultureInfo)
+        {
+            return totalAmount.ToString("C", new CultureInfo(cultureInfo).NumberFormat);
         }
     }
 }
